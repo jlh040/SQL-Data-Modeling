0060ed61dc99d4d64116e9180bb6f1975d096e59
Jeff Hall <jeff.leon.hall@gmail.com>
2021-03-09 00:05:20 -0700

    added a schema for a hospital

---
+++ hospital_schema.sql
@@ -0,0 +1,77 @@
+DROP DATABASE IF EXISTS hospital;
+
+CREATE DATABASE hospital;
+
+\c hospital;
+
+CREATE TABLE doctors (
+    id SERIAL PRIMARY KEY,
+    name_of_doc TEXT NOT NULL
+);
+
+CREATE TABLE patients (
+    id SERIAL PRIMARY KEY,
+    name_of_patient TEXT NOT NULL
+);
+
+CREATE TABLE diseases (
+    id SERIAL PRIMARY KEY,
+    name_of_disease TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE diseases_patients (
+    id SERIAL PRIMARY KEY,
+    patient_id INT REFERENCES patients,
+    disease_id INT REFERENCES diseases
+);
+
+CREATE TABLE doctors_patients (
+    id SERIAL PRIMARY KEY,
+    patient_id INT REFERENCES patients,
+    doctor_id INT REFERENCES doctors
+);
+
+INSERT INTO 
+doctors (name_of_doc)
+VALUES
+('John Wayne'),
+('Sally Moore'),
+('Richard Diesel');
+
+INSERT INTO 
+patients (name_of_patient)
+VALUES
+('Duane Chapman'),
+('Robby Leland'),
+('Lisa Brudi');
+
+INSERT INTO
+diseases (name_of_disease)
+VALUES
+('cancer'),
+('heart disease'),
+('lymphoma');
+
+INSERT INTO 
+diseases_patients (patient_id, disease_id)
+VALUES
+(1,3),
+(1,1),
+(2,3),
+(3,2),
+(2,2);
+
+INSERT INTO
+doctors_patients (patient_id, doctor_id)
+VALUES
+(2,3),
+(1,1),
+(3,1),
+(2,2),
+(1,3);
+
+
+-- See which patients have which diseases.
+SELECT name_of_patient, name_of_disease
+FROM diseases_patients JOIN patients ON patient_id = patients.id
+JOIN diseases ON disease_id = diseases.id;
