511c044a3fa5c27c0bb56eafe5365195ca2efec4
Jeff Hall <jeff.leon.hall@gmail.com>
2021-03-09 19:16:41 -0700

    updated schema for air_traffic.sql

--- air_traffic.sql
+++ air_traffic.sql
@@ -1,37 +1,118 @@
--- from the terminal run:
--- psql < air_traffic.sql
-
 DROP DATABASE IF EXISTS air_traffic;
 
 CREATE DATABASE air_traffic;
 
-\c air_traffic
+\c air_traffic;
 
-CREATE TABLE tickets
+CREATE TABLE flyers
 (
   id SERIAL PRIMARY KEY,
   first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL,
-  seat TEXT NOT NULL,
-  departure TIMESTAMP NOT NULL,
-  arrival TIMESTAMP NOT NULL,
-  airline TEXT NOT NULL,
-  from_city TEXT NOT NULL,
-  from_country TEXT NOT NULL,
-  to_city TEXT NOT NULL,
-  to_country TEXT NOT NULL
+  last_name TEXT NOT NULL
 );
 
-INSERT INTO tickets
-  (first_name, last_name, seat, departure, arrival, airline, from_city, from_country, to_city, to_country)
+CREATE TABLE airlines (
+  id SERIAL PRIMARY KEY,
+  name_of_airline TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE locations (
+  id SERIAL PRIMARY KEY,
+  country TEXT NOT NULL,
+  city TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE flights (
+  id SERIAL PRIMARY KEY,
+  departure TIMESTAMP NOT NULL,
+  arrival TIMESTAMP NOT NULL,
+  airline_id INT REFERENCES airlines(id),
+  from_location_id INT REFERENCES locations(id),
+  to_location_id INT REFERENCES locations(id)
+);
+
+CREATE TABLE flights_flyers (
+  id SERIAL PRIMARY KEY,
+  flyer_id INT REFERENCES flyers(id),
+  flight_id INT REFERENCES flights(id),
+  seat TEXT NOT NULL 
+);
+
+
+INSERT INTO flyers
+  (first_name, last_name)
 VALUES
-  ('Jennifer', 'Finch', '33B', '2018-04-08 09:00:00', '2018-04-08 12:00:00', 'United', 'Washington DC', 'United States', 'Seattle', 'United States'),
-  ('Thadeus', 'Gathercoal', '8A', '2018-12-19 12:45:00', '2018-12-19 16:15:00', 'British Airways', 'Tokyo', 'Japan', 'London', 'United Kingdom'),
-  ('Sonja', 'Pauley', '12F', '2018-01-02 07:00:00', '2018-01-02 08:03:00', 'Delta', 'Los Angeles', 'United States', 'Las Vegas', 'United States'),
-  ('Jennifer', 'Finch', '20A', '2018-04-15 16:50:00', '2018-04-15 21:00:00', 'Delta', 'Seattle', 'United States', 'Mexico City', 'Mexico'),
-  ('Waneta', 'Skeleton', '23D', '2018-08-01 18:30:00', '2018-08-01 21:50:00', 'TUI Fly Belgium', 'Paris', 'France', 'Casablanca', 'Morocco'),
-  ('Thadeus', 'Gathercoal', '18C', '2018-10-31 01:15:00', '2018-10-31 12:55:00', 'Air China', 'Dubai', 'UAE', 'Beijing', 'China'),
-  ('Berkie', 'Wycliff', '9E', '2019-02-06 06:00:00', '2019-02-06 07:47:00', 'United', 'New York', 'United States', 'Charlotte', 'United States'),
-  ('Alvin', 'Leathes', '1A', '2018-12-22 14:42:00', '2018-12-22 15:56:00', 'American Airlines', 'Cedar Rapids', 'United States', 'Chicago', 'United States'),
-  ('Berkie', 'Wycliff', '32B', '2019-02-06 16:28:00', '2019-02-06 19:18:00', 'American Airlines', 'Charlotte', 'United States', 'New Orleans', 'United States'),
-  ('Cory', 'Squibbes', '10D', '2019-01-20 19:30:00', '2019-01-20 22:45:00', 'Avianca Brasil', 'Sao Paolo', 'Brazil', 'Santiago', 'Chile');
+  ('Jennifer', 'Finch'),
+  ('Thadeus', 'Gathercoal'),
+  ('Sonja', 'Pauley'),
+  ('Jennifer', 'Finch'),
+  ('Waneta', 'Skeleton'),
+  ('Thadeus', 'Gathercoal'),
+  ('Berkie', 'Wycliff'),
+  ('Alvin', 'Leathes'),
+  ('Berkie', 'Wycliff'),
+  ('Cory', 'Squibbes');
+
+INSERT INTO airlines
+  (name_of_airline)
+VALUES
+  ('United'),
+  ('British Airways'),
+  ('Delta'),
+  ('TUI Fly Belgium'),
+  ('Air China'),
+  ('American Airlines'),
+  ('Avianca Brasil');
+
+INSERT INTO locations
+  (country, city)
+VALUES
+  ('United States', 'Seattle'),
+  ('United States', 'Washington DC'),
+  ('United States', 'Chicago'),
+  ('United States', 'Las Vegas'),
+  ('United States', 'Charlotte'),
+  ('United States', 'Cedar Rapids'),
+  ('United States', 'Los Angeles'),
+  ('United States', 'New Orleans'),
+  ('United States', 'New York'),
+  ('United Kingdom', 'London'),
+  ('Japan', 'Tokyo'),
+  ('France', 'Paris'),
+  ('China', 'Beijing'),
+  ('Brazil', 'Sao Paolo'),
+  ('Chile', 'Santiago'),
+  ('Mexico', 'Mexico City'),
+  ('Morocco', 'Casablanca'),
+  ('UAE', 'Dubai');
+
+INSERT INTO flights
+  (departure, arrival, airline_id, from_location_id, to_location_id)
+VALUES
+  ('2018-04-08 09:00:00', '2018-04-08 12:00:00', 1, 2, 1),
+  ('2018-12-19 12:45:00', '2018-12-19 16:15:00', 2, 10, 9),
+  ('2018-01-02 07:00:00', '2018-01-02 08:03:00', 3, 7, 4),
+  ('2018-04-15 16:50:00', '2018-04-15 21:00:00', 3, 1, 15),
+  ('2018-08-01 18:30:00', '2018-08-01 21:50:00', 4, 11, 16),
+  ('2018-10-31 01:15:00', '2018-10-31 12:55:00', 5, 17, 12), 
+  ('2019-02-06 06:00:00', '2019-02-06 07:47:00', 1, 9, 5),
+  ('2018-12-22 14:42:00', '2018-12-22 15:56:00', 6, 6, 3),
+  ('2019-02-06 16:28:00', '2019-02-06 19:18:00', 6, 5, 8),
+  ('2019-01-20 19:30:00', '2019-01-20 22:45:00', 7, 13, 14);
+
+INSERT INTO flights_flyers
+  (flyer_id, flight_id, seat)
+VALUES
+  (1, 10, '11B'),
+  (5, 8, '6X'),
+  (8, 7, '70N'),
+  (4, 4, '20P'),
+  (3, 3, '33B'),
+  (7, 1, '65C'),
+  (4, 9, '45V'),
+  (3, 1, '15A'),
+  (2, 2, '90D'),
+  (10, 6, '80A');
+
+
+-- Display the name of the flyer, along with their departure time and desination city
